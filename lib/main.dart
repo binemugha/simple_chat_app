ebd5e56a205a045437992464fbf1e95044558497
Benjamin <binemugha@gmail.com>
2024-12-22 11:43:08 +0000

    added firebase auth and core

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_chat_app/auth/login_or_register.dart';
+import 'package:simple_chat_app/firebase_options.dart';
 import 'package:simple_chat_app/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
